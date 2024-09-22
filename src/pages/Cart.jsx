61d04d5a67caa4e54173d7e06f45330bd4234d43
AnjanSthaa <anjanshrestha588@gmail.com>
2024-09-22 19:38:24 +0545

    Done

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import LoginForm from './LoginForm'
 import NavBar from '../components/NavBar'
+import Signup from './Signup'
 
 const Cart = () => {
   return (
     <div>
       <NavBar />
+      <Signup />
       <LoginForm />
     </div>
   )
